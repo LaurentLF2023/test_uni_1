fd0404cf796c8084a5206c0b307f8795a19e815d
Laurent Le Foulgoc <laurent.le-foulgoc@epitech.eu>
2023-12-06 09:24:26 +0100

    premier passage

--- test_uni_1/src/Main.java
+++ test_uni_1/src/Main.java
@@ -1,17 +1,57 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Entrée with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        LocalTime now = java.time.LocalTime.now();
+        LocalTime noon = LocalTime.parse("12:00:00");
+        int timeDiff = now.compareTo(noon);
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        boolean isMorning = false;
+
+        if (timeDiff < 0) {
+            isMorning = true;
+            System.out.print("Good morning and welcome! \n");
+        } else {
+            System.out.print("Good afternoon and welcome! \n");
         }
+
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        while (Objects.equals(userInput, "")) {
+            userInput = in.nextLine();
+
+        }
+
+        StringBuilder input1 = new StringBuilder();
+        input1.append(userInput);
+        input1.reverse();
+
+        if (input1.toString().equals(userInput)) {
+
+            if (Objects.equals(System.getProperty("user.language"), "fr")) {
+                System.out.print("Bien dit! \n");
+            } else {
+                System.out.print("Well said! \n");
+            }
+        }
+
+        System.out.print(userInput.concat("\n"));
+
+        if (isMorning) {
+            System.out.print("Bonne fin de matinée.");
+        } else {
+            System.out.print("Bonne fin de journée.");
+        }
+
     }
 }
